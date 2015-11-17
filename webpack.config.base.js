b36b711853a448d0da9014f67f28c583dfddc6a3
zilong <jzlxiaohei@163.com>
2015-11-17 17:25:15 +0800

    extends

---
+++ webpack.config.base.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    library: 'Redux',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
