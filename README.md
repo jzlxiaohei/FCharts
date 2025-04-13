9ae4a8169487ec5f3f89d2ebb4396789a7f993e4
jiangzilong.godpig <jiangzilong.godpig@bytedance.com>
2025-04-13 20:43:25 +0800

    chore(readme): clean repo

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ https://jzlxiaohei.github.io/f-chart/test_v2.html
 
 # How to run
 
-- npm install
+- pnpm install
+- npm run build:umd:min
 - npx gulp dev-server
 
 then open `http://localhost:8080/demo/test_v2.html` to see demo
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 //import Layout from './Layout/Layout.js'
-import Chart from './Chart/Chart.js'
-import Constant from './Constant/Constant.js'
-import DefaultStyle from './DefaultStyle/DefaultStyle.js'
-import Extends from '../extends/index'
+import Chart from "./Chart/Chart.js";
+import Constant from "./Constant/Constant.js";
+import DefaultStyle from "./DefaultStyle/DefaultStyle.js";
+import Extends from "./extends/index";
 
-var FCharts={
-    Chart,
-    Constant,
-    DefaultStyle,
-    ChartCreator:Extends
-}
+var FCharts = {
+  Chart,
+  Constant,
+  DefaultStyle,
+  ChartCreator: Extends,
+};
 
 export default FCharts;
